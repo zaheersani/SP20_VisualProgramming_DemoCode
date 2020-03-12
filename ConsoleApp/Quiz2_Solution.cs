4ba850cd1fedda5a0d71826b3d8c702321d7b7fb
zaheersani <sani.zaheer@gmail.com>
2020-03-12 17:25:38 +0500

    WPF - Common Controls (Checkbox, Radio, ComboBox, ListBox, ListView, DatePicker)

--- ConsoleApp/Quiz2_Solution.cs
+++ ConsoleApp/Quiz2_Solution.cs
@@ -6,55 +6,55 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-class Employee
-{
-    public string Name { get; set; }
-    public string Department { get; set; }
-    public List<DeptTask> Tasks { get; set; }
-
-    public DeptTask this[int index]
+    class Employee
     {
-        get
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public List<DeptTask> Tasks { get; set; }
+
+        public DeptTask this[int index]
         {
-            if(index < Tasks.Count)
-                return this.Tasks[index];
-            return new DeptTask();
+            get
+            {
+                if(index < Tasks.Count)
+                    return this.Tasks[index];
+                return new DeptTask();
+            }
+        }
+
+        public DeptTask this[string TaskCode]
+        {
+            get
+            {
+                //foreach (DeptTask item in Tasks)
+                //{
+                //    if (item.Code == TaskCode)
+                //        return item;
+                //}
+                return Tasks.Find(x => x.Description.Contains(TaskCode) && x.Priority <= 1);
+                return Tasks.Find(x => x.Code == TaskCode);
+                return new DeptTask();
+            }
+        }
+
+        public List<DeptTask> this[string Description, bool returnList]
+        {
+            get
+            {
+                return Tasks.FindAll(x => x.Description.Contains(Description));
+            }
         }
     }
-
-    public DeptTask this[string TaskCode]
+    struct DeptTask
     {
-        get
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public DeptTask(string code, int priority, string desc)
         {
-            //foreach (DeptTask item in Tasks)
-            //{
-            //    if (item.Code == TaskCode)
-            //        return item;
-            //}
-            return Tasks.Find(x => x.Description.Contains(TaskCode) && x.Priority <= 1);
-            return Tasks.Find(x => x.Code == TaskCode);
-            return new DeptTask();
+            this.Code = code;
+            this.Priority = priority;
+            this.Description = desc;
         }
-    }
-
-    public List<DeptTask> this[string Description, bool returnList]
-    {
-        get
-        {
-            return Tasks.FindAll(x => x.Description.Contains(Description));
-        }
-    }
-}
-struct DeptTask
-{
-    public string Code { get; set; }
-    public string Description { get; set; }
-    public int Priority { get; set; }
-    public DeptTask(string code, int priority, string desc)
-    {
-        this.Code = code;
-        this.Priority = priority;
-        this.Description = desc;
-    }
     }
 }
---
+++ WPFApp/WPFCommonControls.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for WPFCommonControls.xaml
+    /// </summary>
+    public partial class WPFCommonControls : Window
+    {
+        public WPFCommonControls()
+        {
+            InitializeComponent();
+            ItemCollection collection = this.comboBox.Items;
+            collection = this.listBox.Items;
+            collection = this.listView.Items;
+            //foreach (var item in collection)
+            //{
+            //    if(item is ListBoxItem)
+            //        MessageBox.Show(((ListBoxItem)item).Content.ToString());
+            //}
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(this.comboBox.SelectedValue.ToString());
+        }
+
+        private void DatePicker_CalendarClosed(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.datePicker.SelectedDate.Value.ToString("dddd dd MMMM"));
+        }
+    }
+}
